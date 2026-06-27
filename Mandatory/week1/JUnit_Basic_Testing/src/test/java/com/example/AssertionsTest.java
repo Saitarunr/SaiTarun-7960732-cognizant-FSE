841265100039055b98ecb4c8aceb385e541b07d5
nn <145021663+Saitarunr@users.noreply.github.com>
2026-06-27 10:39:46 +0530

    Update AssertionsTest.java

--- Mandatory/week1/JUnit_Basic_Testing/src/test/java/com/example/AssertionsTest.java
+++ Mandatory/week1/JUnit_Basic_Testing/src/test/java/com/example/AssertionsTest.java
@@ -1,1 +1,24 @@
+package com.example;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class AssertionsTest {
+
+    @Test
+    public void verifyAllAssertions() {
+
+        assertEquals(15, 10 + 5);
+
+        assertTrue(20 > 10);
+
+        assertFalse(5 > 10);
+
+        assertNotNull(new Calculator());
+
+        assertNull(null);
+
+    }
+
+}
