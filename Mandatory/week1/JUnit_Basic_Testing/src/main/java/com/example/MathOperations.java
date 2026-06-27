e846d6355430eced89a7295a345ef1823e1e51a2
nn <145021663+Saitarunr@users.noreply.github.com>
2026-06-27 11:03:02 +0530

    Update MathOperations.java

--- Mandatory/week1/JUnit_Basic_Testing/src/main/java/com/example/MathOperations.java
+++ Mandatory/week1/JUnit_Basic_Testing/src/main/java/com/example/MathOperations.java
@@ -1,11 +1,16 @@
-package com.exercise1;
+package com.example;
 
 public class MathOperations {
 
-    public int add(int firstNumber, int secondNumber) {
-
-        return firstNumber + secondNumber;
-
+    public int square(int number) {
+        return number * number;
     }
 
+    public int cube(int number) {
+        return number * number * number;
+    }
+
+    public boolean isEven(int number) {
+        return number % 2 == 0;
+    }
 }
