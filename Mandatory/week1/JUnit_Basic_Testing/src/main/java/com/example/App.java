5b21f60da7ddf23824313a37fa4b508b0da4e483
nn <145021663+Saitarunr@users.noreply.github.com>
2026-06-27 10:28:33 +0530

    Update and rename readme.txt to App.java

---
+++ Mandatory/week1/JUnit_Basic_Testing/src/main/java/com/example/App.java
@@ -0,0 +1,15 @@
+package com.exercise1;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        Calculator calculator = new Calculator();
+
+        int answer = calculator.performAddition(15, 25);
+
+        System.out.println("Addition Result : " + answer);
+
+    }
+
+}
