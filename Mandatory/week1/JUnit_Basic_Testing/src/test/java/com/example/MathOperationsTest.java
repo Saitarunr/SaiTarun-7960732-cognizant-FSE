4db79b08a0e29a89f50473e42c52e1768cafc325
nn <145021663+Saitarunr@users.noreply.github.com>
2026-06-27 10:40:16 +0530

    Update MathOperationsTest.java

--- Mandatory/week1/JUnit_Basic_Testing/src/test/java/com/example/MathOperationsTest.java
+++ Mandatory/week1/JUnit_Basic_Testing/src/test/java/com/example/MathOperationsTest.java
@@ -1,1 +1,61 @@
+package com.example;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class MathOperationsTest {
+
+    private MathOperations mathOperations;
+
+    @BeforeEach
+    public void initializeObject() {
+
+        mathOperations = new MathOperations();
+
+    }
+
+    @Test
+    public void testSquareOperation() {
+
+        assertEquals(36, mathOperations.square(6));
+
+    }
+
+    @Test
+    public void testCubeOperation() {
+
+        assertEquals(216, mathOperations.cube(6));
+
+    }
+
+    @Test
+    public void testEvenNumber() {
+
+        assertTrue(mathOperations.isEven(12));
+
+    }
+
+    @Test
+    public void testOddNumber() {
+
+        assertFalse(mathOperations.isEven(15));
+
+    }
+
+    @Test
+    public void testSquareOfZero() {
+
+        assertEquals(0, mathOperations.square(0));
+
+    }
+
+    @Test
+    public void testNegativeCube() {
+
+        assertEquals(-125, mathOperations.cube(-5));
+
+    }
+
+}
