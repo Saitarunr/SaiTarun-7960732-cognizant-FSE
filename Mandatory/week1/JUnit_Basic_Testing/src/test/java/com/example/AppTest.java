132d8072e22927cc198725319126bc02a9779968
nn <145021663+Saitarunr@users.noreply.github.com>
2026-06-27 10:39:31 +0530

    Update AppTest.java

--- Mandatory/week1/JUnit_Basic_Testing/src/test/java/com/example/AppTest.java
+++ Mandatory/week1/JUnit_Basic_Testing/src/test/java/com/example/AppTest.java
@@ -1,1 +1,16 @@
+package com.example;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class AppTest {
+
+    @Test
+    public void shouldLoadApplication() {
+
+        assertTrue(true);
+
+    }
+
+}
