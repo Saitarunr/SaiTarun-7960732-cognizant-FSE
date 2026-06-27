cb72ab6b5fdbd5ca1f1e85af9746be88e53912d7
nn <145021663+Saitarunr@users.noreply.github.com>
2026-06-27 10:40:01 +0530

    Update CalculatorTest.java

--- Mandatory/week1/JUnit_Basic_Testing/src/test/java/com/example/CalculatorTest.java
+++ Mandatory/week1/JUnit_Basic_Testing/src/test/java/com/example/CalculatorTest.java
@@ -1,1 +1,47 @@
+package com.example;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class CalculatorTest {
+
+    private Calculator calculator;
+
+    @BeforeEach
+    public void createCalculatorObject() {
+
+        calculator = new Calculator();
+
+    }
+
+    @Test
+    public void testAddition() {
+
+        assertEquals(25, calculator.add(20, 5));
+
+    }
+
+    @Test
+    public void testSubtraction() {
+
+        assertEquals(15, calculator.subtract(20, 5));
+
+    }
+
+    @Test
+    public void testMultiplication() {
+
+        assertEquals(100, calculator.multiply(20, 5));
+
+    }
+
+    @Test
+    public void testDivision() {
+
+        assertEquals(4, calculator.divide(20, 5));
+
+    }
+
+}
