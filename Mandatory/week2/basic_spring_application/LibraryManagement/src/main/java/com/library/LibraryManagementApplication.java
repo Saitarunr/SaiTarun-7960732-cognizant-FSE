4c049622208134fcea794b565ece29b1126632c6
nn <145021663+Saitarunr@users.noreply.github.com>
2026-06-30 09:37:32 +0530

    Update LibraryManagementApplication.java

--- Mandatory/week2/basic_spring_application/LibraryManagement/src/main/java/com/library/LibraryManagementApplication.java
+++ Mandatory/week2/basic_spring_application/LibraryManagement/src/main/java/com/library/LibraryManagementApplication.java
@@ -1,1 +1,23 @@
+package com.library;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.library.service.BookService;
+
+public class LibraryManagementApplication {
+
+    public static void main(String[] args) {
+
+        ClassPathXmlApplicationContext context =
+                new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        BookService service =
+                context.getBean("serviceBean", BookService.class);
+
+        service.startLibraryService();
+
+        context.close();
+
+    }
+
+}
