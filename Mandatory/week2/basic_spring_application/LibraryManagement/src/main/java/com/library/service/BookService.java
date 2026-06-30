37bf9ea22ad23f07a3c235d862b629d4ad222c7f
nn <145021663+Saitarunr@users.noreply.github.com>
2026-06-30 09:37:05 +0530

    Update BookService.java

--- Mandatory/week2/basic_spring_application/LibraryManagement/src/main/java/com/library/service/BookService.java
+++ Mandatory/week2/basic_spring_application/LibraryManagement/src/main/java/com/library/service/BookService.java
@@ -1,1 +1,27 @@
+package com.library.service;
 
+import com.library.repository.BookRepository;
+
+public class BookService {
+
+    private BookRepository repository;
+
+    // Setter Injection
+
+    public void setRepository(BookRepository repository) {
+
+        this.repository = repository;
+
+    }
+
+    public void startLibraryService() {
+
+        System.out.println("Book Service Initialized");
+
+        System.out.println(repository.fetchLibraryRecord());
+
+        System.out.println("Dependency Injection Completed Successfully.");
+
+    }
+
+}
