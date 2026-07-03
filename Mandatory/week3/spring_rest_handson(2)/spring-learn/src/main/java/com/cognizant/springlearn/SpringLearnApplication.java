8f7ba217f42ee824b9055cf2d4e96b6644a3cbe6
nn <145021663+Saitarunr@users.noreply.github.com>
2026-07-03 09:34:03 +0530

    Create SpringLearnApplication.java

---
+++ Mandatory/week3/spring_rest_handson(2)/spring-learn/src/main/java/com/cognizant/springlearn/SpringLearnApplication.java
@@ -0,0 +1,8 @@
+@SpringBootApplication
+public class SpringLearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringLearnApplication.class, args);
+    }
+
+}
