a7d8c0913f6c4e857c221228778575faaf7ae75f
Sai-Tarun <145021663+Saitarunr@users.noreply.github.com>
2026-07-27 12:49:07 +0530

    Update App.js

--- Mandatory/week5/src/labs/Lab2_StudentApp/App.js
+++ Mandatory/week5/src/labs/Lab2_StudentApp/App.js
@@ -2,9 +2,6 @@ import React from "react";
 import Home from "./Components/Home";
 import About from "./Components/About";
 import Contact from "./Components/Contact";
-
-// LAB 2: StudentApp
-// Objective: Create a class component, create multiple components, render them
 function Lab2_StudentApp() {
   return (
     <div>
