efc7bf2b4de24a44c167e79fb0611df1d613fbe5
Sai-Tarun <145021663+Saitarunr@users.noreply.github.com>
2026-07-27 12:51:14 +0530

    Update IndianPlayers.js

--- Mandatory/week5/src/labs/Lab6_CricketApp/IndianPlayers.js
+++ Mandatory/week5/src/labs/Lab6_CricketApp/IndianPlayers.js
@@ -1,16 +1,12 @@
 import React from "react";
 
-// LAB 6: cricketapp - IndianPlayers
-// Objective: Destructuring and the array merge (spread) feature of ES6
 function IndianPlayers() {
   const oddTeamPlayers = ["Rohit Sharma", "Shubman Gill", "Rishabh Pant", "Ravindra Jadeja", "Mohammed Shami", "Kuldeep Yadav"];
   const evenTeamPlayers = ["Virat Kohli", "KL Rahul", "Hardik Pandya", "Axar Patel", "Jasprit Bumrah"];
 
-  // Destructuring the first few players out of each array
   const [oddCaptain, oddViceCaptain, ...oddRest] = oddTeamPlayers;
   const [evenCaptain, evenViceCaptain, ...evenRest] = evenTeamPlayers;
 
-  // Two more squads, merged using the ES6 spread (merge) feature
   const T20players = ["Suryakumar Yadav", "Yashasvi Jaiswal", "Arshdeep Singh"];
   const RanjiTrophyPlayers = ["Sarfaraz Khan", "Yash Dhull", "Rajat Patidar"];
   const mergedSquad = [...T20players, ...RanjiTrophyPlayers];
