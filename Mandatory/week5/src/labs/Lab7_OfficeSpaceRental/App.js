0b4ae92bfab45bee85ba4d8894e2a1efad80c225
Sai-Tarun <145021663+Saitarunr@users.noreply.github.com>
2026-07-27 12:52:19 +0530

    Update App.js

--- Mandatory/week5/src/labs/Lab7_OfficeSpaceRental/App.js
+++ Mandatory/week5/src/labs/Lab7_OfficeSpaceRental/App.js
@@ -1,18 +1,14 @@
 import React from "react";
 
-// LAB 7: officespacerentalapp
-// Objective: JSX elements, attributes, JS expressions in JSX, inline CSS
 const officeImage =
   "https://images.unsplash.com/photo-1497366216548-37526070297c?w=400";
 
-// A single office object
 const office = {
   name: "Skyline Business Hub",
   rent: 75000,
   address: "MG Road, Bengaluru"
 };
 
-// A list of office objects
 const officeList = [
   { id: 1, name: "Tech Park Tower", rent: 45000, address: "Whitefield, Bengaluru" },
   { id: 2, name: "Skyline Business Hub", rent: 75000, address: "MG Road, Bengaluru" },
@@ -23,13 +19,13 @@ const officeList = [
 function Lab7_OfficeSpaceRental() {
   return (
     <div>
-      {/* Element to display the heading of the page */}
+      {}
       <h1>Office Space Rental</h1>
 
-      {/* Attribute used to display an image */}
+      {}
       <img src={officeImage} alt="Office space" width="300" />
 
-      {/* Displaying details of a single office object using JS expressions in JSX */}
+      {}
       <h2>Featured Office</h2>
       <p>Name: {office.name}</p>
       <p>Address: {office.address}</p>
@@ -37,7 +33,7 @@ function Lab7_OfficeSpaceRental() {
         Rent: ₹{office.rent}
       </p>
 
-      {/* Looping through a list of office objects */}
+      {}
       <h2>All Available Offices</h2>
       <ul>
         {officeList.map((item) => (
